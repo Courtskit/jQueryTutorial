8b6d2cca8dd26bd13bbdace84d68708be2d308e8
Courtskit <Courtskit@gmail.com>
2020-05-27 17:46:24 -0700

    practice events

--- jquery.js
+++ jquery.js
@@ -12,3 +12,27 @@ header.css({
 
 //////////////////////////
 $('h1').css({ border: '3px solid magenta' });
+
+var dom = "<div style='text-align: center; border: 2px solid grey'>Document Object Model </div>";
+
+$("#studyDom div").append(dom);
+
+var domSection = $("#studyDom div");
+
+domSection.on('click', function () {
+
+  $(this).css({ 'background': 'pink' });
+
+});
+
+$('h1').click(function () {
+
+  $(this).css({
+    'background': 'lightgrey',
+    'border': '2px solid darkblue',
+    'color': 'cyan'
+  });
+
+  alert('you clicked me');
+
+});
